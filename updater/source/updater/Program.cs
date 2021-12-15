55236bf85046e56c6aadcbb96e8f54c0fa3ccf99
megabyte112 <74556753+megabyte112@users.noreply.github.com>
2021-12-15 11:42:10 +0000

    bug fixes, clean up

--- updater/source/updater/Program.cs
+++ updater/source/updater/Program.cs
@@ -7,33 +7,35 @@ namespace updater
     {
         static void Main()
         {
-            // Remove older version
-            if (File.Exists("..\\megabyte112RP.zip")) {File.Delete("..\\megabyte112RP.zip");}
-
             // find the parent directory, check if it's the resourcepacks folder
             string dir = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
             if (dir.ToString().Substring(dir.Length-13, 13) != "resourcepacks")
             {
-                Console.WriteLine("The updater isn't in the right place.\nMake sure it is only one folder down from \\resourcepacks.\nPress enter to exit.");
+                Console.WriteLine("The updater isn't in the right place.\nMake sure it is in \"\\resourcepacks\\updater\".");
                 Console.ReadLine();
                 Environment.Exit(0);
             }
-
-            // if all is well, begin the download
-            Console.WriteLine("Downloading...");
-            try
+            // Remove older version
+            try 
             {
-                WebClient client = new WebClient();
-                client.DownloadFile("https://github.com/megabyte112/megabyte112-mc-resourcepack/releases/latest/download/megabyte112RP.zip", dir + "\\megabyte112RP.zip");
+                if (File.Exists("..\\megabyte112RP.zip"))
+                {
+                    Console.WriteLine("Removing older version...");
+                    File.Delete("..\\megabyte112RP.zip");
+                }
             }
             catch
             {
-                Console.WriteLine("There was a problem somewhere.\nCheck that the resource pack is not in use in Minecraft.\nFailing that, come talk to me.");
-                Console.WriteLine("\n\nFailed - Enter to exit");
+                Console.WriteLine("\n\nThere was a problem - Check that the resource pack is NOT selected in Minecraft.\nFailing that, come talk to me.");
+                Console.WriteLine("\nFailed - Close and try again");
                 Console.ReadLine();
                 Environment.Exit(0);
             }
-            Console.WriteLine("Success!\nPress enter to close.");
+            // if all is well, begin the download
+            Console.WriteLine("Downloading lastest release...");
+            WebClient client = new WebClient();
+            client.DownloadFile("https://github.com/megabyte112/megabyte112-mc-resourcepack/releases/latest/download/megabyte112RP.zip", dir + "\\megabyte112RP.zip");
+            Console.WriteLine("\n\nSuccess!\nYou can now close the updater.");
             Console.ReadLine();
         }
     }
